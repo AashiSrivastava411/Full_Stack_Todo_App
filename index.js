73803600cda1e5f10ef212870a4f4e44b869f7c9
Aashi Srivastava <aasheesrivastava411@gmail.com>
2026-08-17 19:47:14 +0530

    Move static files into public/ for vercel deployment

--- index.js
+++ index.js
@@ -19,11 +19,9 @@ const app = express();
 const port = process.env.PORT||3010;
 
 app.use(express.json());
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
